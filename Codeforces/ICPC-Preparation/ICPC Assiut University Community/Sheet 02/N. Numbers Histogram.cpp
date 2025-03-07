aed2877e79ca6bd6071e0a08803e9fdb003fafe2
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-03-07 19:58:51 +0600

    Sheet 02 -- N. Numbers Histogram and O. Pyramid

---
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Sheet 02/N. Numbers Histogram.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    char s; cin >> s;
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        for(int i=1; i<=n; i++){
+            cout << s;
+        }
+        cout << endl;        
+    }
+    return 0;
+}
