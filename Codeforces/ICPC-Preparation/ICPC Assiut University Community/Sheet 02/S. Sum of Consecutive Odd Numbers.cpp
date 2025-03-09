91992429a242f533f25851e2b5c47e4ac7b493cd
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-03-09 21:12:35 +0600

    Sheet 02 - S. Sum of Consecutive Odd Numbers

---
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Sheet 02/S. Sum of Consecutive Odd Numbers.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int x, y; cin >> x >> y;
+        int sum = 0;
+        if(x<y){
+            for (int i=x+1; i<y; i++){
+                if(i%2!=0){
+                    sum = sum + i;
+                }
+            }
+        }
+        else{
+            for (int i=y+1; i<x; i++){
+                if(i%2!=0){
+                    sum = sum + i;
+                }
+            }
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
