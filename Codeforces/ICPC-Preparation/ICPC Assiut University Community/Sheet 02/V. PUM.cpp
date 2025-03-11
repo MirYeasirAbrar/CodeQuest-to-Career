19ddd2165c5ae8cf7d68320b4c411abad9c8f835
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-03-11 15:49:06 +0600

    Sheet 02 - V. PUM

---
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Sheet 02/V. PUM.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n; cin >> n;
+    for (int i=1; i<=n; i++){
+        int a=1, b=2, c=3; 
+        string ch = "PUM"; 
+        cout << a << " " << b << " " <<  c << " " << ch << endl;
+        a = a+4;
+        b = b+4;
+        c = c+4;
+    }
+    
+    return 0;
+}
