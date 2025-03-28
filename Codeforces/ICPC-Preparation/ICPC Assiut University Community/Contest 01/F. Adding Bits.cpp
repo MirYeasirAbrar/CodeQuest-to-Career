70d6e7de0a7d3d01324cd6f994c8ed9845102ba9
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-03-28 23:01:58 +0600

    Contest 01 - F. Adding Bits,  Contest 02 - E. Hady Rides the Train

---
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Contest 01/F. Adding Bits.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    unsigned __int32 a, b;
+    cin >> a >> b;
+
+    unsigned __int32 r = a ^ b;
+    cout << r << endl;
+    return 0;
+}
--- Codeforces/ICPC-Preparation/ICPC Assiut University Community/Contest 02/E. Hady Rides the Train.cpp
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Contest 02/E. Hady Rides the Train.cpp
@@ -4,6 +4,23 @@ using namespace std;
 int main() {
     long long id;
     cin >> id;
-    cout << id / 4 << " " << id % 4 << endl;
+    cout << id / 4 << " ";
+    if((id/4)%2 == 0){
+        cout << id % 4 << endl;
+    }
+    else{
+        if(id%4 == 0){
+            cout << 3 << endl;
+        }
+        else if(id%4 == 1){
+            cout << 2 << endl;
+        }
+        else if(id%4 == 2){
+            cout << 1 << endl;
+        }
+        else if(id%4 == 3){
+            cout << 0 << endl;
+        }
+    }
     return 0;
 }
