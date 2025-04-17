2cfad6314a9ef11027e85e1005923ba683a872cb
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-04-17 17:41:36 +0600

    Sheet #4 (Strings) - A
    A. Create A New String

---
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Sheet #4 (Strings)/A_Create_A_New_String.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s, t; cin >> s >> t;
+    cout << s.length() << " " << t.size() << '\n';
+    cout << s << " " << t << '\n';
+
+    return 0;
+}
