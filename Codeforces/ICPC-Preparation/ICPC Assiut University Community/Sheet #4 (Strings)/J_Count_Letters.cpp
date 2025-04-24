dfded32efae6df210ca6c7d4142bacaab3783e1b
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-04-24 21:50:59 +0600

    Create J_Count_Letters.cpp

---
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Sheet #4 (Strings)/J_Count_Letters.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    vector<int> count(26, 0); 
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            count[s[i] - 'a']++; 
+        }
+    }
+
+    for (int i = 0; i < 26; i++) {
+        if(count[i]>0){
+            cout << (char)(i + 'a') << " : " << count[i] << endl;
+        }
+    }
+
+    return 0;
+}
