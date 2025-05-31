52ee97d6b94ef867b23b81495f319f75449332c1
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-05-31 15:08:27 +0600

    758A - Holiday Of Equality

---
+++ Codeforces/Problem Set/758A - Holiday Of Equality.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n; cin >> n; 
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
+
+    int mx = *max_element(a.begin(), a.end());
+    int sum = 0;
+    for(int i=0; i<n; i++) {
+        sum += mx - a[i];
+    }
+
+    cout << sum << '\n';
+    return 0; 
+}
