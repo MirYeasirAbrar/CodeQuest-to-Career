d2abdc8f3cad730fa61df32cd2fcc02ddf42cb11
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-06-06 15:55:39 +0600

    Update X_Comparison.cpp

--- Codeforces/ICPC-Preparation/ICPC Assiut University Community/Sheet #4 (Strings)/X_Comparison.cpp
+++ Codeforces/ICPC-Preparation/ICPC Assiut University Community/Sheet #4 (Strings)/X_Comparison.cpp
@@ -6,16 +6,15 @@ int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
     string str; cin >> str;
-    string s1 = "", s2 = str, s = str;
+    string s = str;
 
-    for(int i=0; i<str.size(); i++){
-        s1 += str[i];
-        s2.erase(0, 1);
+    for(int i = 1; i < str.size(); i++) {
+        string s1 = str.substr(0, i);
+        string s2 = str.substr(i);
         sort(s1.begin(), s1.end());
         sort(s2.begin(), s2.end());
-        s = min(s, s1+s2);
-    }    
-    // sort(s.begin(), s.end());
+        s = min(s, s1 + s2);
+    }
 
     cout << s << endl;
     return 0;
