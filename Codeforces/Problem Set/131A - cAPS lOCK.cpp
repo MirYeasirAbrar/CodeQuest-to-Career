19cce0fc7eba0797f0074be4ae4c1fb86c83cb00
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-06-21 16:54:58 +0600

    131A - cAPS lOCK

---
+++ Codeforces/Problem Set/131A - cAPS lOCK.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    string s;
+    char u;
+    bool c = true;
+    cin >> s;
+
+    for(int i = 1; i < s.length(); i++)
+    {
+        if(islower(s[i]))
+        {
+            c = false;
+        }
+    }
+
+    if(c == true)
+    {
+        for(int j = 0; j < s.length(); j++)
+        {
+            if(islower(s[j]))
+                u = toupper(s[j]);
+            else
+                u = tolower(s[j]);
+            cout << u;
+        }
+    }
+    else
+        cout << s;
+    return 0;
+}
