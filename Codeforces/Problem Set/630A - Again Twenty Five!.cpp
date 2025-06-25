1503eb93f21d63c50e303b9d45a65cb2d312c1e3
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-06-25 19:35:21 +0600

    630A - Again Twenty Five!

---
+++ Codeforces/Problem Set/630A - Again Twenty Five!.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    long long n; cin >> n;
+    cout << 25 << endl;
+    return 0;
+}
