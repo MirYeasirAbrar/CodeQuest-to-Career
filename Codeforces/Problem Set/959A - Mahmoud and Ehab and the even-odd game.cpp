7530377c3e419149f1e567253d7496613cb1239e
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-07-13 21:34:00 +0600

    959A - Mahmoud and Ehab and the even-odd game

---
+++ Codeforces/Problem Set/959A - Mahmoud and Ehab and the even-odd game.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int n; cin >> n;
+    cout << ((n % 2 == 0) ? "Mahmoud" : "Ehab") << endl;
+    return 0;
+}
