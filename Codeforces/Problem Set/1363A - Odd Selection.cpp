0aa903738028adf2966a32e26c7d6e35c10ce2ad
Mir Yeasir Abrar <182193318+MirYeasirAbrar@users.noreply.github.com>
2025-08-08 21:50:38 +0600

    Updated -- 1363A - Odd Selection

--- Codeforces/Problem Set/1363A - Odd Selection.cpp
+++ Codeforces/Problem Set/1363A - Odd Selection.cpp
@@ -1,31 +1,37 @@
 #include <bits/stdc++.h>
 #define endl '\n'
-#define ll long long
+#define int long long
 using namespace std;
 
-int main() {
-    cin.tie(nullptr)->sync_with_stdio(false);
+int main()
+{
+	cin.tie(nullptr)->sync_with_stdio(false);
 
-    int t; cin >> t;
-    while(t--){
-        int n, x;
-        vector<int> a(n);
-        int f[2];
-        f[0] = f[1] = 0;
-    	cin >> n >> x;
-    	for(int i = 1; i <= n; i++)
-    	{
-    		cin >> a[i];
-    		f[a[i] % 2]++;
-    	}
-    	bool flag = 0;
-    	for(int i = 1; i <= f[1] && i <= x; i += 2) //Fix no of odd
-    	{
-    		int have = f[0], need = x - i;
-    		if(need <= f[0])
-    	        flag = 1;
-    	}
-    cout << (flag ? "Yes" : "No") << endl;    
-    }
-    return 0;
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, k;
+		cin >> n >> k;
+		vector<int> v(n);
+		int o = 0, e = 0;
+		for (int i = 0; i < n; i++){
+			cin >> v[i];
+
+			if (v[i] % 2 == 0){
+				e++;
+			}
+			else{
+				o++;
+			}
+		}
+
+		if (o == 0 or (n == k and o % 2 == 0) or (o == n and k % 2 == 0)){
+			cout << "No" << "\n";
+		}
+		else{
+			cout << "Yes" << "\n";
+		}
+	}
+	return 0;
 }
