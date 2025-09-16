5f0f1a95da6f46a51f6fb0ef4ed8de18f2d9254e
Mir Yeasir Abrar <abrar@students.diu.ac>
2025-09-16 22:12:36 +0600

    2148A - Sublime Sequence

---
+++ Codeforces/Problem Set/2148A - Sublime Sequence.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+	ll t;
+	cin >> t;
+	while(t--){
+		int x, n;
+		cin >> x >> n;
+		cout << (n % 2 ? x : 0) << endl;
+	}
+	return 0;
+}
