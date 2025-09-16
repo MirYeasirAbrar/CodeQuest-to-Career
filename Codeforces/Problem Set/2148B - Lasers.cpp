4e636e4918b87ab43fdc05042e8f06e330ae39a6
Mir Yeasir Abrar <abrar@students.diu.ac>
2025-09-16 22:15:47 +0600

    2148B - Lasers

---
+++ Codeforces/Problem Set/2148B - Lasers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+	ll t;
+	cin >> t;
+	while(t--){
+		int n, m, x, y, c;
+        cin >> n >> m >> x >> y;
+        for(int i = 0; i < n + m; i++) cin >> c;
+        cout << n + m << endl;
+	}
+	return 0;
+}
