89b4df6274c13fcd96406bd70dcf82249275da8d
Mir Yeasir Abrar <abrar@students.diu.ac>
2025-09-20 22:38:01 +0600

    Update 2137D - Replace with Occurrences.cpp

--- Codeforces/Problem Set/2137D - Replace with Occurrences.cpp
+++ Codeforces/Problem Set/2137D - Replace with Occurrences.cpp
@@ -1,46 +1,38 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (auto &i : a)
-            cin >> i;
-        map<int, int> mp;
-        map<int, vector<int>> pos;
-        for (int i = 0; i < n; i++)
-        {
-            mp[a[i]]++;
-            pos[a[i]].push_back(i);
-        }
-        for (auto &i : mp)
-        {
-            if (i.second % i.first)
-            {
-                cout << "-1\n";
-                break;
-            }
-        }
-        map<int, int> x, y;
-        vector<int> ans(n);
-        int cur = 1;
-        for (int i = 0; i < n; i++)
-        {
-            if (x[a[i]] == 0)
-            {
-                y[a[i]] = cur++;
-                x[a[i]] = a[i];
-            }
-            ans[i] = y[a[i]];
-            x[a[i]]--;
-        }
-        for (auto i : ans)
-            cout << i << " ";
-        cout << "\n";
-    }
+int t,n;
+const int N=2e5+10;
+
+int main(){
+	cin >> t;
+	while(t--){
+		cin >> n;
+		vector<vector<int>> g(n+1);
+		vector<int> ans(n+1);
+		for(int i=1;i<=n;i++){
+			int x;cin>>x;
+			g[x].push_back(i);
+		}
+		int cnt = 0;
+		bool flag = true;
+		for(int i = 1; i <= n; i++){
+			if(g[i].size() % i != 0){
+				cout << "-1\n";
+				flag = false;
+				break;
+			}
+			else{
+				for(int j=0;j<g[i].size();j++){
+					if(j%i==0) cnt++;
+					ans[g[i][j]] = cnt;
+				}
+			}
+		}
+		if(flag){
+			for(int i=1;i<=n;i++){
+				cout << ans[i] << " ";
+			}
+			cout << "\n";
+		}
+	}
 }
