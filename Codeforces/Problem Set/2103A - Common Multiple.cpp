deccbd35b75a2c940730f435d43294c8b7843cea
Mir Yeasir Abrar <abrar@students.diu.ac>
2025-09-29 21:29:52 +0600

    2103A - Common Multiple

---
+++ Codeforces/Problem Set/2103A - Common Multiple.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        set<int> s;
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            s.insert(x);
+        }
+        cout << s.size() << endl;
+    }
+
+    return 0;
+}
