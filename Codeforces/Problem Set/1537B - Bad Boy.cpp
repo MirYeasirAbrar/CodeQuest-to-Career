a533c4181f523c468fe3a7f59e2c477866baac0e
Mir Yeasir Abrar <abrar@students.diu.ac>
2025-10-15 18:23:44 +0600

    1537B - Bad Boy

---
+++ Codeforces/Problem Set/1537B - Bad Boy.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m, i, j;
+        cin >> n >> m >> i >> j;
+        cout << 1 << ' ' << 1 << ' ' << n << ' ' << m << endl;
+    }
+
+    return 0;
+}
