8aa1cffdb39841afad968d97077982bacc562eb9
Mir Yeasir Abrar <abrar@students.diu.ac>
2025-10-26 21:46:39 +0600

    1471A - Strange Partition

---
+++ Codeforces/Problem Set/1471A - Strange Partition.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x, a, k, mx, mn;
+        cin >> n >> x;
+        mx = mn = 0;
+        for (k = 1; k <= n; k++)
+        {
+            cin >> a;
+            mx += (a + x - 1) / x;
+            mn += a;
+        }
+        cout << (mn + x - 1) / x << " " << mx << endl;
+    }
+
+    return 0;
+}
