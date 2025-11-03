28e6812df7e78362136fd7fab1f8e8ec077cf398
Mir Yeasir Abrar <abrar@students.diu.ac>
2025-11-03 09:27:14 +0600

    2123C - Prefix Min and Suffix Max

---
+++ Codeforces/Problem Set/2123C - Prefix Min and Suffix Max.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define ll long long
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m = INT_MAX;
+        cin >> n;
+        vector<int> p(n), s(n), v(n);
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> v[i];
+            m = min(v[i], m);
+            p[i] = m;
+        }
+        m = INT_MIN;
+        for (int i = n - 1; i >= 0; --i)
+        {
+            m = max(m, v[i]);
+            s[i] = m;
+        }
+        for (int i = 0; i < n; ++i)
+        {
+            cout << ((v[i] == p[i] || v[i] == s[i]) ? "1" : "0");
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
